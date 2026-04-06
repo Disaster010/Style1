773423bf6a5a1e84eb9ac8532e12efc66f4cb2a7
Disaster010 <luangabrielf6@gmail.com>
2026-04-06 17:00:41 -0300

    style1

---
+++ style.js
@@ -0,0 +1,13 @@
+function corAzul(){
+    let elemento = document.getElementById('style-text')
+    elemento.style.backgroundColor = 'blue'
+}
+function transparente(){
+    document.getElementById('style-text').style.backgroundColor = 'transparent'
+}
+function corRoxa(){
+    document.getElementById('style-text').style.color = 'purple'
+}
+function corPreta(){
+    document.getElementById('style-text').style.color = 'black'
+}
